5f4a7e7603b118f273649bc825ca0628d554868e
domespa <dumiii1988@gmail.com>
2024-12-17 12:56:13 +0100

    init project - snacks done

---
+++ snack-1/script.js
@@ -0,0 +1,13 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach (name => {
+    console.log(name)
+});
+
+
+//const nameElm = document.getElementById("aaa");  
+//const namesDiv = names.join(', ');
+//nameElm.textContent = namesDiv;
